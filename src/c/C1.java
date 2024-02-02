4b8e3d4c5a549dfa2f847d024682e3ea14994868
Temirlan744 <temirlanbakytbekuulu27@gmail.com>
2024-02-02 18:26:47 +0600

    C1-commit-3

--- src/c/C1.java
+++ src/c/C1.java
@@ -4,4 +4,7 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void sayHello(){
+        System.out.println("hello");
+    }
 }
