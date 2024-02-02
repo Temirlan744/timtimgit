55972dfcebc1565b7e6628298f7f880ac5a1b1c8
Temirlan744 <temirlanbakytbekuulu27@gmail.com>
2024-02-02 18:28:47 +0600

    Revert "C1-commit-updated"
    This reverts commit 4b8e3d4c5a549dfa2f847d024682e3ea14994868.

--- src/c/C1.java
+++ src/c/C1.java
@@ -4,7 +4,4 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
-    public static void sayHello(){
-        System.out.println("hello");
-    }
 }
